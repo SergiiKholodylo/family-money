63da39e23d7ebad0b8597829bb6ce1535ad823c2
SergiiKholodylo <46851200+SergiiKholodylo@users.noreply.github.com>
2019-07-10 14:26:46 +0300

    Refactoring StartPage

--- FamilyMoneyLib/StartPage.cs
+++ FamilyMoneyLib/StartPage.cs
@@ -10,56 +10,15 @@ namespace FamilyMoneyLib
     {
         long Id { set; get; }
     }
-
-    public class DbStorage<T> where T : IIdBased, new()
+    public interface ITransactionItem
     {
-        private readonly List<T> _storage = new List<T>();
-        public long Add(T t)
-        {
-            var id = GetId();
-            t.Id = id;
-            _storage.Add(t);
-            return id;
-        }
-
-        public T Get(long id)
-        {
-            var found = _storage.FirstOrDefault(x => x.Id == id);
-            if(found == null)
-                return new T();
-            return found;
-
-        }
-
-        public void Update(T t)
-        {
-            Delete(t.Id);
-            _storage.Add(t);
-        }
-
-        public void Delete(long id)
-        {
-            var toDelete = Get(id);
-            _storage.Remove(toDelete);
-        }
-
-        public IEnumerable<T> GetAll()
-        {
-            return _storage; 
-        }
-
-        private long GetId()
-        {
-            var rnd = new Random();
-            while (true)
-            {
-                var newId = (long)rnd.Next(0xFFFF);
-                if (_storage.Exists(x => x.Id == newId))
-                    continue;
-                return newId;
-            }
-        }
+         long Id { set; get; }
+         Category Category { set; get; }
+         string Name { set; get; }
+         decimal Total { set; get; }
     }
+
+   
 
 
     public class Account:IIdBased
@@ -79,15 +38,6 @@ namespace FamilyMoneyLib
         }
 
     }
-
-    public interface ITransactionItem
-    {
-         long Id { set; get; }
-         Category Category { set; get; }
-         string Name { set; get; }
-         decimal Total { set; get; }
-    }
-
     public class Transaction:ITransactionItem, IIdBased
     {
         public static long NewTransactionId { get; } = -1;
@@ -108,8 +58,6 @@ namespace FamilyMoneyLib
             Id = NewTransactionId;
         }
     }
-
-
     public class Category : IIdBased
     {
 
@@ -124,6 +72,7 @@ namespace FamilyMoneyLib
             Id = NewCategoryId;
         }
     }
+
 
     public class TransactionStorage
     {
@@ -212,6 +161,57 @@ namespace FamilyMoneyLib
         public IEnumerable<Account> GetAllAccounts()
         {
             return DataBaseConnector.GetAll();
+        }
+    }
+
+
+    public class DbStorage<T> where T : IIdBased, new()
+    {
+        private readonly List<T> _storage = new List<T>();
+        public long Add(T t)
+        {
+            var id = GetId();
+            t.Id = id;
+            _storage.Add(t);
+            return id;
+        }
+
+        public T Get(long id)
+        {
+            var found = _storage.FirstOrDefault(x => x.Id == id);
+            if(found == null)
+                return new T();
+            return found;
+
+        }
+
+        public void Update(T t)
+        {
+            Delete(t.Id);
+            _storage.Add(t);
+        }
+
+        public void Delete(long id)
+        {
+            var toDelete = Get(id);
+            _storage.Remove(toDelete);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _storage; 
+        }
+
+        private long GetId()
+        {
+            var rnd = new Random();
+            while (true)
+            {
+                var newId = (long)rnd.Next(0xFFFF);
+                if (_storage.Exists(x => x.Id == newId))
+                    continue;
+                return newId;
+            }
         }
     }
 }
