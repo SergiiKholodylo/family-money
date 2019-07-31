d83641031f50766c96067abeaa50d7a40a6afaf8
SergiiKholodylo <46851200+SergiiKholodylo@users.noreply.github.com>
2019-07-31 11:23:19 +0300

    Added BarCode class and some tests

---
+++ FamilyMoneyLib.NetStandard/Bases/BarCode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyMoneyLib.NetStandard.Bases
+{
+    public class BarCode
+    {
+        private const int BarCodeWeightFactor = 10000;
+
+        private readonly string _code;
+
+        private readonly bool _isWeight;
+
+        private readonly int _numberOfDigitsForWeight;
+
+        public BarCode(string code, bool isWeight = false, int numberOfDigitsForWeight = 0)
+        {
+            if (code.Length < numberOfDigitsForWeight) throw new ArgumentException();
+
+            _code = code;
+            _isWeight = isWeight;
+            _numberOfDigitsForWeight = numberOfDigitsForWeight;
+        }
+        public decimal GetWeightKg()
+        {
+            if(!_isWeight) return 0;
+            if (_code.Length < _numberOfDigitsForWeight) return 0;
+            var lastNSymbols = _code.Substring(_code.Length - _numberOfDigitsForWeight);
+            var weightKg = Convert.ToDecimal(lastNSymbols)/ BarCodeWeightFactor;
+            return weightKg;
+        }
+
+        public string GetProductBarCode()
+        {
+            if (!_isWeight) return _code;
+            var productBarCodeLength = _code.Length - _numberOfDigitsForWeight;
+            var productBarCode = _code.Substring(0,productBarCodeLength);
+            return productBarCode;
+        }
+
+
+    }
+}
---
+++ FamilyMoneyTest/Bases/BarCodeTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FamilyMoneyLib.NetStandard.Bases;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Bases
+{
+    [TestClass]
+    public class BarCodeTest
+    {
+        private const string SilpoZefir = "2710041003240";
+
+        [TestMethod]
+        public void GetWeightTest()
+        {
+            var barcode = new BarCode(SilpoZefir, true, 6);
+
+
+            var weight = barcode.GetWeightKg();
+
+
+            Assert.AreEqual(0.324m,weight);
+        }
+
+
+        [TestMethod]
+        public void GetProductBarCodeTest()
+        {
+            var barcode = new BarCode(SilpoZefir, true, 6);
+
+
+            var productBarCode = barcode.GetProductBarCode();
+
+
+            Assert.AreEqual("2710041",productBarCode);
+        }
+    }
+}
