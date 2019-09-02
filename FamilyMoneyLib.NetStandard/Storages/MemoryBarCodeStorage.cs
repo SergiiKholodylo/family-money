ab94f23b8278f297a48b0805529956ab6b078c9e
SergiiKholodylo <46851200+SergiiKholodylo@users.noreply.github.com>
2019-09-02 15:26:54 +0300

    IdAble refactoring

--- FamilyMoneyLib.NetStandard/Storages/MemoryBarCodeStorage.cs
+++ FamilyMoneyLib.NetStandard/Storages/MemoryBarCodeStorage.cs
@@ -9,17 +9,11 @@ namespace FamilyMoneyLib.NetStandard.Storages
     public class MemoryBarCodeStorage : BarCodeStorageBase
     {
 
-        private readonly ITransactionStorage _transactionStorage;
-
         private readonly MemoryStorageBase _storageEngine = new MemoryStorageBase();
 
-
-        public MemoryBarCodeStorage(IBarCodeFactory factory, ITransactionStorage transactionStorage):base(factory)
+        public MemoryBarCodeStorage(IBarCodeFactory factory, ITransactionStorage transactionStorage):base(factory,transactionStorage)
         {
-            _transactionStorage = transactionStorage;
         }
-
-
 
         public override void UpdateBarCode(IBarCode barCode)
         {
@@ -39,17 +33,6 @@ namespace FamilyMoneyLib.NetStandard.Storages
             }
         }
 
-        public override ITransaction CreateBarCodeBasedTransaction(string barCode)
-        {
-            var transaction = GetBarCodeTransaction(barCode);
-            if (transaction == null) return transaction;
-
-            transaction.Timestamp = DateTime.Now;
-            transaction.Id = 0;
-            var newTransaction = _transactionStorage.CreateTransaction(transaction);
-            return newTransaction;
-        }
-
         public override void DeleteAllData()
         {
             _storageEngine.DeleteAllData();
@@ -65,11 +48,5 @@ namespace FamilyMoneyLib.NetStandard.Storages
             return _storageEngine.GetAll().Cast<IBarCode>();
         }
 
-        public override ITransaction GetBarCodeTransaction(string barCode)
-        {
-            //var foundBarCode = _storage.FirstOrDefault(x => x.Key.Equals(barCode)).Value;
-            var foundBarCode = GetAllBarCodes().FirstOrDefault(x => x.GetProductBarCode().Equals(barCode));
-            return foundBarCode?.Transaction;
-        }
     }
 }
